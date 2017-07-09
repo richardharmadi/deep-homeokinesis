c7aaac7cb48a65944149cedcde4f7a8d6fa08690
Richard Dharmadi <richardharmadi@richards-mbp-2.home>
2017-07-09 13:36:58 +0100

    debug

--- selforg/examples/deep-homeokinesis/deep-homeokinesis.cpp
+++ selforg/examples/deep-homeokinesis/deep-homeokinesis.cpp
@@ -61,7 +61,7 @@ int main(){
   memset(motors,0,sizeof(double)*MNumber);  // clear motors
 
   // the robot is here respresented by the function myrobot
-  for(int i=0; i < 10; i++){
+  for(int i=0; i < 20; i++){
     // call robot with motors and receive sensors 
     myrobot(sensors, SNumber, motors, MNumber);
     cout << i << " Sensor X0: " << sensors[0] << ", " << sensors[1] << endl;
