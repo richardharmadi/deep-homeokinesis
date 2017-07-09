baf7cb73ceadfd2cabeb7898a8ce2fc79ea97274
Richard Dharmadi <richardharmadi@richards-mbp-2.home>
2017-07-09 14:12:16 +0100

    check step

--- selforg/controller/stackinvertmotornstep.cpp
+++ selforg/controller/stackinvertmotornstep.cpp
@@ -70,6 +70,7 @@ void StackInvertMotorNStep::step(const sensor* x_, int number_sensors,
   }else{ 
     for(int i=0;i<controllers.size();i++){
       controllers[i+1].stepNoLearning(x_,number_sensors,y_,number_motors);
+    }
   }
 }
 
