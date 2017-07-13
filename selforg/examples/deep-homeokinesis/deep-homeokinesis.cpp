159b4a9629cabe2a30063dfc4343cca8bae3f004
Richard Dharmadi <richardharmadi@richards-mbp-2.home>
2017-07-13 09:08:44 +0100

    add 5 & 10 layers experiment

--- selforg/examples/deep-homeokinesis/deep-homeokinesis.cpp
+++ selforg/examples/deep-homeokinesis/deep-homeokinesis.cpp
@@ -29,16 +29,48 @@ void myrobot(double* sensors, int sensornumber, const double* motors, int motorn
 }
 
 int main(){
-  inputvalue.open("input.csv");
-  outputvalue.open("output.csv");
+  // ---------- 2 layers ---------------------//
+  //inputvalue.open("2layers_input.csv");
+  //outputvalue.open("2layers_output.csv");
+
+  // ---------- 5 layers ---------------------//
+  inputvalue.open("5layers_input.csv");
+  outputvalue.open("5layers_output.csv");
+  
+  // ---------- 10 layers --------------------//
+  //inputvalue.open("10layers_input.csv");
+  //outputvalue.open("10layers_output.csv");
 
   StackInvertMotorNStep* main_controller = new StackInvertMotorNStep(buffersize,3); // initialise with buffer size 50 and 2 layers
+  
+  // ---------- 2 layers ---------------------//
   InvertMotorNStep* controller0 = new InvertMotorNStep();
   InvertMotorNStep* controller1 = new InvertMotorNStep();
-  //InvertMotorNStep* controller2 = new InvertMotorNStep();
+
   main_controller->addLayer(controller0);
   main_controller->addLayer(controller1);
-  //main_controller->addLayer(*controller2);
+  // ---------- 5 layers ---------------------//
+  InvertMotorNStep* controller2 = new InvertMotorNStep();
+  InvertMotorNStep* controller3 = new InvertMotorNStep();
+  InvertMotorNStep* controller4 = new InvertMotorNStep();
+
+  main_controller->addLayer(controller2);
+  main_controller->addLayer(controller3);
+  main_controller->addLayer(controller4);
+  
+  // ---------- 10 layers --------------------//
+  //InvertMotorNStep* controller5 = new InvertMotorNStep();
+  //InvertMotorNStep* controller6 = new InvertMotorNStep();
+  //InvertMotorNStep* controller7 = new InvertMotorNStep();
+  //InvertMotorNStep* controller8 = new InvertMotorNStep();
+  //InvertMotorNStep* controller9 = new InvertMotorNStep();
+  
+  //main_controller->addLayer(controller5);
+  //main_controller->addLayer(controller6);
+  //main_controller->addLayer(controller7);
+  //main_controller->addLayer(controller8);
+  //main_controller->addLayer(controller9);
+  
   main_controller->init(2,2); // initialise with 2 motors and 2 sensors
 
   //controller->setParam("epsA",0.01); // set parameter epsA (learning rate for Model A)
@@ -53,7 +85,16 @@ int main(){
 
   memset(motors,0,sizeof(double)*MNumber);  // clear motors
   inputvalue << "Sensor X0,Motor Y0,\n";
-  outputvalue << "Sensor X1,Motor Y1,\n";
+  
+  // ---------- 2 layers ---------------------//
+  //outputvalue << "Sensor X1,Motor Y1,\n";
+  
+  // ---------- 5 layers ---------------------//
+  outputvalue << "Sensor X1,Motor Y1,Sensor X2,Motor Y2,Sensor X3,Motor Y3, Sensor X4,Motor Y4,\n";
+  
+  // ---------- 10 layers ---------------------//
+  //outputvalue << "Sensor X1,Motor Y1,Sensor X2,Motor Y2,Sensor X3,Motor Y3, Sensor X4,Motor Y4,Sensor X5,Motor Y5,Sensor X6,Motor Y6,Sensor X7,Motor Y7,Sensor X8,Motor Y8,Sensor X9,Motor Y9\n";
+
   // the robot is here respresented by the function myrobot
   for(int i=0; i < 1000; i++){
     // call robot with motors and receive sensors 
@@ -87,21 +128,42 @@ int main(){
       }
       cout << i << " Sensor X1: " << nextsensor[0][0] << ", " << nextsensor[0][1] << endl;
       cout << i << " Motor Y1: " << nextmotor[0][0] << ", " << nextmotor[0][1] << endl;
-      outputvalue << nextsensor[0][0] << "," << nextmotor[0][0] << ",\n";
-    }
-
-   
-    /*
       cout << i << " Sensor X2: " << nextsensor[1][0] << ", " << nextsensor[1][1];
       cout << i << " Motor Y2: " << nextmotor[1][0] << ", " << nextmotor[1][1];
-
-
       cout << i << " Sensor X3: " << nextsensor[2][0] << ", " << nextsensor[2][1];
-      cout << i << " Motor Y3: " << nextmotor[2][0] << ", " << nextmotor[2][1];
-    */   
+      cout << i << " Motor Y3: " << nextmotor[2][0] << ", " << nextmotor[2][1];   
+      cout << i << " Sensor X4: " << nextsensor[3][0] << ", " << nextsensor[3][1];
+      cout << i << " Motor Y4: " << nextmotor[3][0] << ", " << nextmotor[3][1];   
+
+      // cout << i << " Sensor X5: " << nextsensor[4][0] << ", " << nextsensor[4][1] << endl;
+      // cout << i << " Motor Y5: " << nextmotor[4][0] << ", " << nextmotor[4][1] << endl;
+      // cout << i << " Sensor X6: " << nextsensor[5][0] << ", " << nextsensor[5][1];
+      // cout << i << " Motor Y6: " << nextmotor[5][0] << ", " << nextmotor[5][1];
+      // cout << i << " Sensor X7: " << nextsensor[6][0] << ", " << nextsensor[6][1];
+      // cout << i << " Motor Y7: " << nextmotor[6][0] << ", " << nextmotor[6][1];   
+      // cout << i << " Sensor X8: " << nextsensor[7][0] << ", " << nextsensor[7][1];
+      // cout << i << " Motor Y8: " << nextmotor[7][0] << ", " << nextmotor[7][1];   
+      // cout << i << " Sensor X9: " << nextsensor[8][0] << ", " << nextsensor[8][1];
+      // cout << i << " Motor Y9: " << nextmotor[8][0] << ", " << nextmotor[8][1];   
+      
+      // outputvalue << nextsensor[0][0] << "," << nextmotor[0][0] << ",\n"; // for 2 layers purpose
+
+      outputvalue << nextsensor[0][0] << "," << nextmotor[0][0] << ","; // for 5 layers purpose
+      outputvalue << nextsensor[1][0] << "," << nextmotor[1][0] << ","; 
+      outputvalue << nextsensor[2][0] << "," << nextmotor[2][0] << ","; 
+      outputvalue << nextsensor[3][0] << "," << nextmotor[3][0] << ",\n";
+
+      // outputvalue << nextsensor[0][0] << "," << nextmotor[0][0] << ","; // for 10 layers purpose
+      // outputvalue << nextsensor[1][0] << "," << nextmotor[1][0] << ","; 
+      // outputvalue << nextsensor[2][0] << "," << nextmotor[2][0] << ","; 
+      // outputvalue << nextsensor[3][0] << "," << nextmotor[3][0] << ","; 
+      // outputvalue << nextsensor[4][0] << "," << nextmotor[4][0] << ","; 
+      // outputvalue << nextsensor[5][0] << "," << nextmotor[5][0] << ","; 
+      // outputvalue << nextsensor[6][0] << "," << nextmotor[6][0] << ","; 
+      // outputvalue << nextsensor[7][0] << "," << nextmotor[7][0] << ","; 
+      // outputvalue << nextsensor[8][0] << "," << nextmotor[8][0] << ",\n"; 
+    }
   }
-  //cout << " Sensor X1: " << nextsensor[0][0] << ", " << nextsensor[0][1] << endl;
-  //cout << " Motor Y1: " << nextmotor[0][0] << ", " << nextmotor[0][1] << endl;
   delete main_controller;
   delete controller0;
   delete controller1;
