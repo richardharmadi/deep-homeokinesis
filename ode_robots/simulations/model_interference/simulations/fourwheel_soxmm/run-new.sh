c4838dd012e86f227f3f1d06be2cd0aeb24f1086
Richard Dharmadi <richad@ip-172-31-13-160.eu-west-1.compute.internal>
2017-09-03 15:26:54 +0000

    new data 30minutes simulations

--- ode_robots/simulations/model_interference/simulations/fourwheel_soxmm/run-new.sh
+++ ode_robots/simulations/model_interference/simulations/fourwheel_soxmm/run-new.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-for layer in 1 3 5
+for layer in 1 2 3 5
 do
-    for trial in {1..20}
+    for playground in 0.2 0.75
     do
-        ./start -playground 0 -terrain 0 -threads 1 -log log-0-$layer-$trial.txt -simtime 60 -nographics -layer $layer
+        for trial in {1..20}
+        do
+            ./start -playground $playground -terrain 1 -threads 1 -log log-$playground-$layer-$trial.txt -simtime 30 -nographics -layer $layer
+        done
     done
 done
